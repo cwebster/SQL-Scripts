0b9c3d5e38a18bde26ad74819b544768fec249d6
laptop <laptop@62005UHB.xuhb.nhs.uk>
2019-07-08 08:59:57 +0100

    Initial commit

--- workload_by_set.sql
+++ workload_by_set.sql
@@ -1,10 +1,18 @@
+-- this does not appear to work 14-6-2019
+
 SELECT DISTINCT
-	TS.Test_Code, TS.Test_Exp, DAI.Date_Authorised, RS.Set_Code, REQ.Location, REQ.Specimen_Number, LOC.Unit_Location_Group_Exp
+	TS.Test_Code, 
+	TS.Test_Exp, 
+	DAI.Date_Authorised, 
+	REQ.Location, 
+	COUNT(REQ.Specimen_Number), 
+	LOC.Unit_Location_Group_Exp
 FROM
 	Test_Result TS, Result_Set RS,Request REQ, Date_Authorised_Index DAI, Ref_Location LOC
 WHERE
-	({fn MONTH(DAI.Date_Authorised)} in (10,11)) AND
-	DAI.Discipline ='C' AND
+    DATEDIFF('D',Date_Received_Index.Date_Received,current_timestamp) < 7
+    AND Date_Received_Index.Discipline IN ('C','H') AND
 	TS.Result_Set_Row_ID = RS.Result_Set_Row_ID AND
 	RS.Request_Row_ID = REQ.Request_Row_ID AND
 	REQ.Location_Row_ID = LOC.Location_Row_ID
+GROUP BY TS.Test_Code

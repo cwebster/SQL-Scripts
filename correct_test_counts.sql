534250b0617a4e63a85d218aa53164facc7de645
Craig Webster <Craig.Webster@uhb.nhs.uk>
2020-01-10 09:36:02 +0000

    working

--- correct_test_counts.sql
+++ correct_test_counts.sql
@@ -1,29 +1,26 @@
-SELECT 
-		Test_Code, 
+SELECT 	Test_Code, 
 		COUNT(Test_Code) As Workload,
-		Lab_Section,
-		Disc, 
-		Analytical_Category, 
+		Quarter,
 		Unit_Location_Group
+		
 FROM
 		(SELECT 
             Result_Set.Set_Code,
             Date_Received_Index.Date_Received,
             Date_Received_Index.Specimen_Number,
-            Date_Received_index.Discipline,
+            Date_Received_index.Discipline AS Disc,
             Test_Result.Test_Code,
             Test_Result.Test_Result_Row_ID,
             Test_Result.Test_Row_ID,
             Test_Result.Results_Sequence_No,
-            Result_Set.Result_Set_Row_ID,
-  			Result_Set.Result_Set_Row_ID, 
-  			Test_Result.Test_Row_ID, 
+   			Test_Result.Test_Row_ID, 
   			Test_Result.Test_Exp,
   			Ref_Set.Lab_Section,
-            Date_Received_Index.Discipline AS Disc,
+            Date_Received_Index.Discipline,
             Request.Analytical_Category,
             Ref_Location.Lab_Statistics_Group_Code,
-            Ref_Location.Unit_Location_Group 
+            Ref_Location.Unit_Location_Group,
+            DATEPART('quarter', Date_Received_Index.Date_Received) AS Quarter
 		FROM 
             Result_Set
             LEFT JOIN Test_Result ON Result_Set.Result_Set_Row_ID = Test_Result.Result_Set_Row_ID
@@ -31,14 +28,9 @@ FROM
             INNER JOIN iLabTP.Request ON (Result_Set.Request_Row_ID = Request.Request_Row_ID)
             INNER JOIN iLabTP.Ref_Location ON (iLabTP.Ref_Location.Location_Row_ID = Request.Location_Row_ID)
             INNER JOIN iLabTP.Ref_Set ON (Result_Set.Set_Row_ID = Ref_Set.Set_Row_ID)
-
-		GROUP BY Date_Received_Index.Specimen_Number, Test_Result.Test_Code)
+		GROUP BY 
+			Date_Received_Index.Specimen_Number, Test_Result.Test_Code)
 WHERE
 		Date_Received >= '2019-04-01'
-		AND Date_Received < '2019-04-02'
-		AND Disc = 'C' 
-GROUP BY 	Test_Code, 
-			Lab_Section, 
-			Disc, 
-			Analytical_Category, 
-			Unit_Location_Group
+		AND Date_Received < '2019-12-31'
+GROUP BY 	Unit_Location_Group, Quarter, Test_Code
